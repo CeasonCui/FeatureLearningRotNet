53e7a3383afb6cb2de1f93921012a348143b37a0
Sykie Chen <chen17@coder17.com>
2019-03-27 17:34:16 +0800

    optimized download script output

--- downImageNet.sh
+++ downImageNet.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo installing python packages
 sudo pip install -r requirements.txt
+echo fixing package bug
 wget https://gist.githubusercontent.com/SykieChen/62fe1c15dfc864af862e8fd4c7b2eed3/raw/a37f73ca64efa68e09ee254054651bdb21bddaca/functional.py
 sudo mv functional.py /usr/local/lib/python2.7/dist-packages/torchvision/transforms/functional.py
 
+echo downloading trained results
 mkdir -p experiments
 cd experiments
 
@@ -15,19 +18,21 @@ wget --load-cookies cookies.txt -O $filename \
      'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
 rm -f confirm.txt cookies.txt
 
-tar -xvf ImageNet_RotNet_AlexNet.tar --one-top-level
+tar -xf ImageNet_RotNet_AlexNet.tar --one-top-level
 rm *.tar
 cd ..
 mkdir -p datasets
 cd datasets
 
+echo downloading places205 csv
 mkdir -p Places205
 cd Places205
 wget -c http://data.csail.mit.edu/places/places205/trainvalsplit_places205.tar.gz
-tar -xzvf trainvalsplit_places205.tar.gz
+tar -xzf trainvalsplit_places205.tar.gz
 rm *.gz
 cd ..
 
+echo downloading imagenet 2012 training data part 3
 mkdir -p IMAGENET
 cd IMAGENET
 mkdir -p ILSVRC2012
@@ -43,12 +48,14 @@ wget --load-cookies cookies.txt -O $filename \
 rm -f confirm.txt cookies.txt
 
 # wget -c http://www.image-net.org/challenges/LSVRC/2012/nnoupb/ILSVRC2012_img_train_t3.tar
-tar -xvf ILSVRC2012_img_train_t3.tar -C ./train/
+tar -xf ILSVRC2012_img_train_t3.tar -C ./train/
 cd train
-for f in *.tar; do tar -xvf "$f" --one-top-level; done
+for f in *.tar; do tar -xf "$f" --one-top-level; done
 rm *.tar
 cd ..
 rm *.tar
+
+echo downloading imagenet 2012 validation data
 mkdir -p val/val
 cd val/val
 
@@ -61,5 +68,5 @@ wget --load-cookies cookies.txt -O $filename \
 rm -f confirm.txt cookies.txt
 # wget -c http://www.image-net.org/challenges/LSVRC/2012/nnoupb/ILSVRC2012_img_val.tar
 
-tar -xvf ILSVRC2012_img_val.tar
+tar -xf ILSVRC2012_img_val.tar
 rm *.tar
