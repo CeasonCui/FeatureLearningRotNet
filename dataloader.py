fix: rotation for different size images

dataloader.py
@@ -244,7 +244,12 @@ def rotate_img(img, rot):  # 改这儿！！
     # angle>0表示逆时针旋转；angle<0表示顺时针旋转
     _pil_img = transforms.functional.to_pil_image(img)
     _r_pil_img = transforms.functional.rotate(_pil_img, rot)
-    return np.array(_r_pil_img)
+    # 0.78 is zoom ratio for maxmium 20 degrees
+    # calculated from sqrt(1+tan(theta)*tan(theta))/(1+tan(theta))
+    _ratio = 0.78
+    _w,_ = _r_pil_img.size
+    center_crop = transforms.CenterCrop(int(_w * _ratio))
+    return np.array(center_crop(_r_pil_img))
 
 
 class DataLoader(object):  # DataLoader 是 torch 给你用来包装你的数据的工具.他们帮你有效地迭代数据
@@ -266,8 +271,6 @@ class DataLoader(object):  # DataLoader 是 torch 给你用来包装你的数据
         mean_pix = self.dataset.mean_pix
         std_pix = self.dataset.std_pix
         self.transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.CenterCrop(174),
             transforms.ToTensor(),  # 将numpy转换成矩阵
             transforms.Normalize(mean=mean_pix, std=std_pix)
         ])
@@ -295,10 +298,10 @@ class DataLoader(object):  # DataLoader 是 torch 给你用来包装你的数据
                     self.transform(rotate_img(img0,  20)),
                     self.transform(rotate_img(img0, -20))
                 ]
-                # # WIP: test code
-                # for i,img in enumerate(rotated_imgs):
-                #     transforms.functional.to_pil_image(img).save(str(i) + ".png")
-                # input("saved")
+                # WIP: test code
+                for i,img in enumerate(rotated_imgs):
+                    transforms.functional.to_pil_image(img).save(str(i) + ".png")
+                raw_input("saved")
 
                 # rotated_imgs = [
                 #     # self.transform(img0),