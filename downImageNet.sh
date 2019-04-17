e5c29aa45fc4a1ae3f8e07d40177b1951c9bef51
Sykie Chen <chen17@coder17.com>
2019-04-18 00:56:24 +0800

    removed unused cifar downloader

--- downImageNet.sh
+++ downImageNet.sh
@@ -31,18 +31,6 @@ cd ..
 mkdir -p datasets
 cd datasets
 
-echo downloading CIFAR10 csv
-mkdir -p CIFAR
-cd CIFAR
-export fileid=1OYAo6yGbT1EFMnX8f2loXx8qm-2BJ89x
-export filename=test_batch
-wget -q --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- \
-     | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
-wget -q --show-progress --load-cookies cookies.txt -O $filename \
-     'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
-rm -f confirm.txt cookies.txt
-cd ..
-
 echo downloading places205 csv
 mkdir -p Places205
 cd Places205
